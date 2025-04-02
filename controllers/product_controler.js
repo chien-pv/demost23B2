50697501f8a5e89d57b1b242b75ee799f7370517
chien-pv <phamvanbk11@gmail.com>
2025-04-02 14:26:45 +0700

    demo page

--- controllers/product_controler.js
+++ controllers/product_controler.js
@@ -20,7 +20,24 @@ const Product = require("../model/product");
 
 class ProductController {
   static async index(req, res) {
-    const products = await Product.find();
+    let q = req.query.q;
+    let page = parseInt(req.query.page);
+    let skip = (page - 1) * 5;
+    console.log(page);
+
+    //page = 1 ==> skip =0
+    //page =  2 ==> skip = 5
+    // page = 3 ==> skip = 10
+    //page = 4 ==> skip =15
+    // page = n ==> skip = (n-1)*5
+
+    let products;
+    if (q) {
+      products = await Product.find({ name: { $regex: q, $options: "i" } });
+    } else {
+      products = await Product.find().skip(skip).limit(5);
+    }
+
     res.render("product", { products });
   }
 }
