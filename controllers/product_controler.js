3a765c4f70a901641d37ad03b3afb47d30cf5c65
chien-pv <phamvanbk11@gmail.com>
2025-04-05 07:41:39 +0700

    demo add

--- controllers/product_controler.js
+++ controllers/product_controler.js
@@ -1,36 +1,14 @@
-const categories = [
-  {
-    id: 1,
-    name: "Điện thoại",
-    description: "Các dòng điện thoại thông minh và phụ kiện",
-  },
-  {
-    id: 2,
-    name: "Laptop",
-    description: "Laptop các hãng khác nhau",
-  },
-  {
-    id: 3,
-    name: "Phụ kiện",
-    description: "Tai nghe, sạc, cáp,...",
-  },
-];
-
 const Product = require("../model/product");
 
 class ProductController {
+  static async new(req, res) {
+    res.render("product_new");
+  }
+
   static async index(req, res) {
     let q = req.query.q;
     let page = parseInt(req.query.page);
     let skip = (page - 1) * 5;
-    console.log(page);
-
-    //page = 1 ==> skip =0
-    //page =  2 ==> skip = 5
-    // page = 3 ==> skip = 10
-    //page = 4 ==> skip =15
-    // page = n ==> skip = (n-1)*5
-
     let products;
     if (q) {
       products = await Product.find({ name: { $regex: q, $options: "i" } });
@@ -39,6 +17,13 @@ class ProductController {
     }
 
     res.render("product", { products });
+  }
+
+  static async delete(req, res) {
+    console.log(req.params.id);
+    let id = req.params.id;
+    let product = await Product.deleteOne({ _id: id });
+    res.redirect("/products");
   }
 }
 
--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,8 @@ const ProductController = require("../controllers/product_controler");
 const router = express.Router();
 
 router.get("/", ProductController.index);
+router.get("/new", ProductController.new);
+
+router.get("/delete/:id", ProductController.delete);
 
 module.exports = router;
